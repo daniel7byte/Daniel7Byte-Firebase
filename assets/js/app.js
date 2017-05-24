2b7a2730fe299a2af39a8d94bc1c16312b3eb7ac
Jose Daniel Posso Garcia <daniel7byte@gmail.com>
2017-05-24 16:15:08 -0500

    Fix - function (delete){ ... }

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+(function() {
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyAch1GAPuio3tQnlvWpbr18Z6Yg4Icm-MQ",
+    authDomain: "daniel7byte-7f2e7.firebaseapp.com",
+    databaseURL: "https://daniel7byte-7f2e7.firebaseio.com",
+    projectId: "daniel7byte-7f2e7",
+    storageBucket: "daniel7byte-7f2e7.appspot.com",
+    messagingSenderId: "1003921223767"
+  };
+  firebase.initializeApp(config);
+
+} ());
